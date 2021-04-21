88e71d1d5c8832a33efe77e897e36f5d7f61e85b
thatsjd <junaiddotcom@outlook.com>
2021-04-21 22:48:50 +0200

    Completed webpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [query,setQuery]= useState('');
+  const [weather,setWeather]= useState({});
+  const api_key="1b3937bbf9df0eeeb58c6c502d4d31c7";
+  const api_base="https://api.openweathermap.org/data/2.5/weather";
+  const pritify_date=(d)=>{
+    let result= new Intl.DateTimeFormat("en-GB",{
+      year: "numeric",
+      month: "long",
+      day:"2-digit",
+      weekday:"long"
+    }).format(d).toString();
+    return "" + result;
+  };
+
+const search=evt=>{
+  if (evt.key === "Enter" ){
+    console.log(api_base+'?q='+ query+'&units=metric&appid='+ api_key)
+  fetch(api_base+'?q='+ query+'&units=metric&appid='+ api_key)
+  .then(response => response.json())
+  .then(result=> {
+    setQuery('');
+    setWeather(result);
+  });
+  }
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className={(typeof weather.main != 'undefined') ? ( (weather.main.temp < 15) ? ("App") : ("App warm-bg")) : ("App warm-bg")}>
+      <main className="main">
+      <div className="bg">
+        <div className="search">
+          <input className="searchbar" 
+          type="search" 
+          placeholder="Search..." 
+          onChange={e=> setQuery(e.target.value)}
+          value={query} 
+          onKeyPress={search} />
+        </div>
+        { (typeof weather.main !='undefined') ? (
+        <div className="content-box">
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{pritify_date(new Date())}</div>
+          </div>
+          <div className="weather-box">  
+            <div className="temp">{Math.round(weather.main.temp)}°C</div>
+            <div className="weather">{weather.weather[0].main}
+            </div>
+          </div>
+        </div>
+        ) : ('')}
+      </div>
+      </main>
     </div>
   );
 }
